5ece4f61818874fe377b35f115148d540ced3c28
DabTheMatt <71525403+DabTheMatt@users.noreply.github.com>
2021-06-14 22:48:59 +0200

    crazy sun

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import Main from "./components/Main";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div>
-      Hello from App
-      <Main name={"Maciek"}/>
-    </div>
+    <AppWrapper>
+      
+      <Main />
+    </AppWrapper>
   );
 }
+
+export const AppWrapper = styled.div`
+background: linear-gradient(0deg, #faa307, #023e8a);
+height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,37 +1,120 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import styled from "styled-components";
 
 class Main extends Component {
-    state={
-        hello: "Witaj",
-        bye: "Żegnaj",
-        actual: true,
-        display: ""
-    }
+  state = {};
 
-    handleClick = () => {
-        if (this.state.actual) {
-        this.setState({
-            actual: !this.state.actual,
-            display: this.state.hello
-        })
-        console.log("prawda");
-    } else {
-        this.setState({
-            actual: !this.state.actual,
-            display: this.state.bye
-        })
-        console.log("fałsz");
-    }
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.display} {this.props.name}
-                <br/><button onClick={this.handleClick}>Zmień</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <MainWrapper>
+        <div className="sea">
+            <table cellspacing="0">
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+      </MainWrapper>
+    );
+  }
 }
+
+export const MainWrapper = styled.div`
+  margin: 0 auto;
+  height: 400px;
+  width: 400px;
+  background: radial-gradient(#ffba08, #f48c06);
+  color: black;
+border-radius: 50%;
+box-shadow: 0px 0px 175px #dc2f02;
+position: relative;
+z-index: 0;
+
+.sea {
+    content: "";
+    height: 50vh;
+    width: 100vw;
+    background-image: radial-gradient(farthest-corner at 50% 0%,
+      #ffba08 0%, #faa307 20%,#023e8a  60%, #03045e 100%);
+    position: absolute;
+    top: 18vh;
+    left: -117%;
+    perspective: 100px;
+    transform: rotateX(45deg);
+}
+
+table {
+    transform-style: preserve-3d;
+}
+
+tr {
+    width: 100vw;
+    height: 10vh;
+    border: 1px solid white;
+}
+
+td {
+    width: 40vw;
+    border: 1px solid white;
+    margin: 0px;
+}
+`;
 
 export default Main;

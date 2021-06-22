b437ee124c8de55de16dae0485891faad846e0fb
DabTheMatt <71525403+DabTheMatt@users.noreply.github.com>
2021-06-22 23:22:31 +0200

    multi actions

--- src/components/Video.js
+++ src/components/Video.js
@@ -12,7 +12,9 @@ class Video extends Component {
     started: true,
     progress: 0,
     duration: 0,
-    shotsArray: [{ time: 0 }],
+    shotsArray: [],
+    shotColor: "",
+    shotSize: "",
     counter: 0,
     onAxisPositon: "",
     order: ""
@@ -58,7 +60,7 @@ class Video extends Component {
     });
   };
 
-  handleShot = () => {
+  handleShot = (color, size) => {
 
     if (!this.state.running) {
       return;
@@ -67,7 +69,9 @@ class Video extends Component {
       const shot = {
         time: this.state.progress.toFixed(3),
         position: this.state.onAxisPositon,
-        order: this.state.shotsArray.length
+        order: this.state.shotsArray.length,
+        shotColor: color,
+        shotSize: size
       };
       let tempArray = this.state.shotsArray;
       this.setState({
@@ -92,45 +96,25 @@ class Video extends Component {
               onProgress={(e) => this.progress(e)}
               onPlay={this.start}
               onPause={this.pause}
-              url="https://www.youtube.com/watch?v=RdSmokR0Enk"
+              url="https://www.youtube.com/watch?v=G0QGi7_KWFE"
             />
-          </VideoWrapper>
-
-          <div className="timeDisplay">
-            <h1>Actual Video Time (AVT)</h1>
-            <h2>{this.state.progress.toFixed(0)} s</h2>
-            <h1>Video duration</h1>
-            <h2>{this.state.duration} s</h2>
-            <h1>Actual Shots Count:</h1>
-            <h2>{this.state.shotsArray.length}</h2>
-
-            <button id="count" onClick={this.handleShot}>
-              +1
-            </button>
-
-            <div>
-              {this.state.shotsArray.map((el) => {
-                return <div>{el.time}</div>;
-              })}
-            </div>
-          </div>
-        </VideoPageWrapper>
-        <GraphWrapper>
+            <GraphWrapper>
           <div className="graphBcg">
             <div className="graph">
               <div className="axisX">
                 <div className="startTime">0</div>
                 <div className="endTime">{this.state.duration}</div>
+                <div className="shotOnAxis" style={{background: "black"}}></div>
               {this.state.shotsArray.map((el)=>{
                   return(
-                      <div className="shotOnAxis" style={{left: `${el.position-(0.3*el.order)}%`}}></div>
+                      <div className="shotOnAxis" style={{left: `${el.position-(0.25*el.order)}%`, background: `${el.shotColor}`, height: `${el.shotSize}`}}></div>
                   )
               })}
                 
 
                 <div
                   className="playHead"
-                  style={{ left: `${this.state.onAxisPositon}%` }}
+                  style={{ left: `${this.state.onAxisPositon+0.2}%` }}
                 >
                   <div className="timeOnAxis">
                     {this.state.progress.toFixed(0)} s
@@ -140,6 +124,33 @@ class Video extends Component {
             </div>
           </div>
         </GraphWrapper>
+          </VideoWrapper>
+
+          <div className="timeDisplay">
+            <h1>Actual Video Time (AVT)</h1>
+            <h2 style={{color: "#ae2012"}}>{this.state.progress.toFixed(0)} s</h2>
+            <h1>Video duration</h1>
+            <h2>{this.state.duration} s</h2>
+            <h1>Actual Shots Count:</h1>
+            <h2 style={{color: "#ee9b00"}}>{this.state.shotsArray.length}</h2>
+
+            <button id="countGreen" onClick={(green, big)=>this.handleShot("#0a9396", "120px")}>
+              +1
+            </button>
+            <button id="countWhite" onClick={(white, small)=>this.handleShot("#94d2bd", "80px")}>
+              +1
+            </button>
+            <button id="countOrange" onClick={(blue, medium)=>this.handleShot("#ee9b00", "100px")}>
+              +1
+            </button>
+            
+
+            <div className="info">
+              <p>If the action happens too fast remember that you can use the clip's settings to slow it down.</p>
+            </div>
+          </div>
+        </VideoPageWrapper>
+        
       </OuterWrapper>
     );
   }
@@ -157,22 +168,72 @@ export const VideoPageWrapper = styled.div`
   }
   .timeDisplay {
     width: 20vw;
+    height: 100vh;
     min-width: 250px;
+    
     color: white;
     text-align: center;
     background: black;
+    font-weight: 200;
+
+    button  {
+      width: 60%;
+      height: 80px;
+      font-size: 2rem;
+      font-weight: 500;
+      font-family: 'Oswald', sans-serif;
+      opacity: 0.8;
+      border: none;
+      margin: 0.1rem;
+    }
+
+    #countGreen {
+      background: #0a9396;
+      opacity: 0.6;
+    }
+
+    #countWhite {
+      background: #94d2bd;
+    }
+
+    #countOrange {
+      background: #ee9b00;
+    }
+
+    h1, h2, h3 {
+      font-weight: 200;
+    }
+
+    h1 {
+      font-size: 1.5rem;
+    }
+
+    h2 {
+      font-weight: 500;
+    }
+
+    .info {
+      width: 80%;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+    }
+
+    p {
+      text-align: right;
+    }
   }
 `;
 
 export const VideoWrapper = styled.div`
-  height: 80vh;
+  height: 60vh;
   width: 80vw;
   background: black;
 `;
 
 export const GraphWrapper = styled.div`
   height: 20vh;
-  width: 100%;
+  width: 80vw;
   background: black;
   color: white;
   display: flex;
@@ -180,33 +241,38 @@ export const GraphWrapper = styled.div`
   justify-content: center;
   .graphBcg {
     height: 85%;
-    width: 95%;
-    background: grey;
+    width: 100%;
+    background: black;
     position: relative;
     display: flex;
     justify-content: center;
     align-items: center;
+    
+    z-index: 0;
   }
   .axisX {
-    width: 90vw;
-    height: 0.1vh;
+    width: 78vw;
+    height: 0.01vh;
     background: white;
-    z-index: 1;
+    z-index: 0;
     position: relative;
-    
+    opacity: 1;
   }
   .playHead {
-    height: 2vh;
-    background: red;
-    width: 0.3%;
+    height: 90px;
+    background: #ae2012;
+    width: 0.4%;
     position: relative;
-    top: -8vh;
+    top: -200px;
+    opacity: 1;
+    transition: 1s linear;
+    z-index: 0;
   }
   .timeOnAxis {
     width: 5vw;
     position: relative;
-    top: 3vh;
-    left: -1.5vw;
+    top: 5vh;
+    left: -0.5vw;
   }
 
   .startTime {
@@ -214,22 +280,34 @@ export const GraphWrapper = styled.div`
     position: relative;
     top: -5vh;
     left: -0.3vw;
+    z-index: 4;
   }
   .endTime {
     width: auto;
     position: relative;
     top: -7vh;
-    left: 88vw;
+    left: 77vw;
+    z-index: 1;
   }
 
   .shotOnAxis {
-    height: 4vh;
-    background: green;
-    width: 0.3%;
+    height: 120px;
+    
+    width: 0.25%;
     position: relative;
-    top: -5vh;
+    top: -88px;
     z-index: 3;
     display: inline-block;
+    opacity: 0.6;
+    transition: 0.2s ;
+  }
+
+  .shotOnAxis:hover {
+    
+  }
+
+  .shotTimeVertical {
+    
   }
 `;
 
